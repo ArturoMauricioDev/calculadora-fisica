4ed6c2f9e504db060f70549b0e6b148f5d837565
Arturo <atercerosb@univalle.edu>
2021-09-23 12:30:34 -0400

    [add] area del cuadrado

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
-var areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
+const perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
+const areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
 
+var calcularAreaCuadrado = () => {
+
+    const ladoDelCuadrado = document.querySelector("#lado-cuadrado").value;
+    const resultado = areaDelCuadrado(ladoDelCuadrado)
+    document.querySelector("#resultado").innerHTML = resultado
+}
 
 
 
