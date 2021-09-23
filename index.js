be5f9d0cb8da5d4313ec13ff72564e8757ce9063
Arturo <atercerosb@univalle.edu>
2021-09-22 20:59:54 -0400

    maquetando

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello world')
+function areaDelCuadrado(ladoDelCuadrado) {
+    const area = ladoDelCuadrado * ladoDelCuadrado;
+    return area
+}
+
+const perimetroDelCuadrado = ladoDelCuadrado => ladoDelCuadrado * 4;
+
+
+// const ladoDelCuadrado = 6;
+// const PI = 3.1416;
+// let variableQueCambia
+// const area = lado * lado;
+// const perimetro = lado * 4;
+
+// console.log(`Area: ${area}`);
+// console.group({ area: area, perimetro: perimetro })
+
+
+
+
+
+
